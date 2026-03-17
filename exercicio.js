adecac44a827b08d5266c190ce00370b2e9176af
FranciscoSplash <franciscomauricio796@gmail.com>
2026-03-17 15:24:47 -0300

    :tada: init: Primeiro commit realizado.

---
+++ exercicio.js
@@ -0,0 +1,72 @@
+
+    //Exercicio numero 1
+    // let nome="Francisco";
+    // let idade = 26;
+    // let profissao ="Estudante";
+    // let cidade ="Santo Adre"
+
+    // console.log(`meu nome é ${nome}, tenho ${idade}, trabalho como ${profissao} e moro em ${cidade}`)
+
+    // Exercicio numero 2
+
+    // let produto ="notebook";
+    // let preco = 2.799;
+    // let quantidade = 3
+
+    // console.log(`comprei ${quantidade} unidades de ${produto} por ${preco} cada`)
+
+    //Exercicio numero 3
+
+//  let valor =10
+//  let valor2 = 20
+ 
+//  console.log(valor + valor2);
+//  console.log(valor - valor2);
+//  console.log(valor * valor2);
+//  console.log(valor / valor2);
+
+  //Exercicio numero 4
+// let nome ="Francisco";
+// let idade = 20;
+// let jaEstudou = true;
+// let vaiViajar=null;
+// let gostasDoSenai;
+
+
+// console.log(nome, typeof(nome))
+// console.log(idade, typeof(idade))
+// console.log(jaEstudou, typeof(jaEstudou))
+// console.log(vaiViajar, typeof(vaiViajar))
+// console.log(gostasDoSenai, typeof(gostasDoSenai))
+
+//Exercicio numero 5
+
+// let numero ='100'
+
+// let numeroConv= Number(numero);
+
+// console.log(typeof(numero));
+// console.log(typeof(numeroConv));
+
+// numero= String(numeroConv)
+// console.log(typeof(numeroConv))
+// console.log(typeof(numero));
+
+//Desafio Final
+
+
+// let nome = "Francisco";
+// let idade = 26;
+// let altura = 1.71;
+// let peso =61;
+// let cidade = "Santo Andre";
+// let profissao = "Estudante";
+
+
+// console.log("=======Cadastro do usuario=======")
+// console.log(`Nome : ${nome}`);
+// console.log(`idade : ${idade}`);
+// console.log(`Altura : ${altura}`);
+// console.log(`Peso : ${peso}`);
+// console.log(`Cidade : ${cidade}`);
+// console.log(`Profissao : ${profissao}`)
---
+++ script.js
@@ -0,0 +1,104 @@
+/*const idade= 30;
+console.log(idade);
+
+
+
+idade = 20
+console.log(idade);
+
+
+let nome= "Soba";
+console.log(nome);   
+
+
+var cidade= "Sao Caetano";
+
+nome="Francisco";
+console.log(nome) ;
+
+
+
+console.log(cidade); */
+
+//Tipos de dados
+/* string, Number, Bolleano, undefined, null
+
+
+let mensagem= 'frango';
+let quanidade= 5;
+let temperatura= 28.5
+let estaChovendo= true;
+let taSol=false;
+
+console.log(typeof(mensagem))
+console.log(typeof(taSol))
+console.log(`${mensagem} de ${quanidade} e ${temperatura}`)
+
+
+let pedido
+let endereco=null
+
+console.log(pedido)
+
+console.log(endereco)*/
+
+//Conversao dos tipos de Dados
+
+
+// console.log('5' + 3);
+// console.log('5' - 3);
+
+// //Conversao explicita
+
+// let numero ='43'
+// let num=100
+
+// let numeroConvertido= Number(numero);
+// let numConvertido = String(num)
+// console.log(numeroConvertido)
+// console.log(typeof(numeroConvertido))
+
+// console.log(typeof(numConvertido))
+
+
+
+// let trueConvertido= Number(true);
+// let falseConvertido= Number(false);
+
+// console.log(trueConvertido)
+// console.log(falseConvertido)
+
+
+
+
+// // exemplos  de (<; >)
+// console.log (10>5);     //verdadeiro
+// console.log (10<5)      //falso
+
+// //Exemplos de  (== e ===)
+// console.log ('10'== 10)     // ignora o tipo (true)
+// console.log('10'===10)      //verifica o tipo (false)
+
+
+// //Exemplos do (!= e !==)
+// console.log('10'!=10)   // ignora o tipo (false)
+// console.log('10'!==10)  //verifica o tipo (true)
+
+
+
+//Operadores MAtematicos=> sao usados para fazer calculos
+
+/* soma => +;
+    sub => -;
+    miltiplcacao => *
+    divisao => / 
+    potenciacao **
+    resto da divisao % 
+    */
+
+
+    // let a = 4
+    // let b = 2
+
+    // console.log(a+b)
+
